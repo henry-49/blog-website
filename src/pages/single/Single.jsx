1a5146158d0f2ded3279167f53c71891ae557b42
henry-49 <henryswebdevelopment@gmail.com>
2023-02-19 09:38:12 +0100

    add single page and single post to project

---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,12 @@
+import SideBar from "../../components/sidebar/SideBar";
+import SinglePost from "../../components/singlePost/singlePost";
+import "./single.css";
+
+export default function Single() {
+  return (
+    <div className="single">
+        <SinglePost />
+        <SideBar />
+    </div>
+  );
+}
